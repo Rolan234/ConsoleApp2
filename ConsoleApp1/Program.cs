ebb3ce5bd4a7d19f5ae5f388cfe664cb362b2bdc
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-09 12:03:05 +0200

    Задание 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,29 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //int[] array = new int[6];
-            //for (int i = 0; i<6; i++)
-            //{
-            //    Console.Write($"array [{i}]");
-            //    array[i] = int.Parse(Console.ReadLine());
-            //}
-            //for (int i=0; i<6; i++)
-            //{
-            //    Console.WriteLine($"array[{i}]={array[i]}");
-            
-            
-        int[] array = new int[12];
-        Random rnd = new Random();
-        for (int i=0; i<12; i++)
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            double[] array = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++ )
             {
-                array[i] = rnd.Next(163, 190);
-            }
-        for (int i=0; i<12; i++)
-            {
+                array[i] = -50 + 100 * rnd.NextDouble();
                 Console.WriteLine($"array[{i}]={array[i]}");
             }
-
-            Console.Read();
+            Console.WriteLine($"array[0]={array[0]}\t array[{n-1}]=[array[{n-1}]");
+            Console.ReadLine();
         }
 
     }

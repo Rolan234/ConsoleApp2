1f32df8c99c00d21bc147014f059fc4e3457caba
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-09 10:50:24 +0200

    Задание 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] array = new int[6];
+            for (int i = 0; i<6; i++)
+            {
+                Console.Write($"array [{i}]");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int[] array = new int[6];
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
         }
+
     }
 }
